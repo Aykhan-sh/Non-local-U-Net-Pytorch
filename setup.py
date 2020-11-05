45b5c9aeaafa5b52a3542bd4598d0e73c6226399
Aykhan <a.turlasov@gmail.com>
2020-11-05 12:57:45 +0600

    setup.py added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='nonlocalunet',
+    version='',
+    packages=['model'],
+    url='https://github.com/Aykhan-sh/Non-local-U-Net-Pytorch',
+    license='',
+    author='Aykhan-sh',
+    author_email='',
+    description=''
+)
