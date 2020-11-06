backprop fixed

model/blocks.py
@@ -44,11 +44,11 @@ class GlobalAggregationBlock(nn.Module):
         queryes = queryes.flatten(start_dim=2, end_dim=-1)
         keys = self.conv_1_ck(x).flatten(start_dim=2, end_dim=-1)
         values = self.conv_1_cv(x).flatten(start_dim=2, end_dim=-1)
-        queryes = queryes.transpose(2, 1).contiguous()
+        queryes = queryes.transpose(2, 1)
         attention = torch.matmul(queryes, keys) / (self.ck ** 0.5)
         attention = self.softmax(attention)
         # TODO add dropout
-        values = values.transpose(2, 1).contiguous()
+        values = values.transpose(2, 1)
         output = torch.matmul(attention, values)
         output = self.conv_1_co(output.view(batch, self.cv, dq, hq, wq))
         return output